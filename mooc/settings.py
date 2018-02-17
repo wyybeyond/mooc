ede160a85461776db76006b39a23fccf401fc727
wyybeyond <wyybeyond@qq.com>
2018-02-17 16:07:59 +0800

    完成USerProfile models书写。makemigrations & migrate 建表成功。

--- mooc/settings.py
+++ mooc/settings.py
@@ -35,7 +35,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'users',
 ]
+AUTH_USER_MODEL = 'users.UserProfile'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
