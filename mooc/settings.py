3a7b6ba7fcbb12baa3e3a08a0a02a2c33baf78a5
wyybeyond <wyybeyond@qq.com>
2018-02-18 13:59:22 +0800

    设计完成课程app中四张数据表: 课程，章节，视频，资源。

--- mooc/settings.py
+++ mooc/settings.py
@@ -35,7 +35,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'users',
+    'apps.users',
 ]
 AUTH_USER_MODEL = 'users.UserProfile'
 
