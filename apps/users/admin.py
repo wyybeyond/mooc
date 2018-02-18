588cad2e157b1c21fda8b597fef49f52249efae2
wyybeyond <wyybeyond@qq.com>
2018-02-18 15:21:08 +0800

    admin中添加管理器&注册。时区,语言,utc(False).数据库中选项参数。

--- apps/users/admin.py
+++ apps/users/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import UserProfile
 
-# Register your models here.
+# 写一个用户的管理器，采用mode+Admin的命名方式
+class UserProfileAdmin(admin.ModelAdmin):
+    pass
+
+# 将UserProfile注册到admin中
+admin.site.register(UserProfile, UserProfileAdmin)
+
